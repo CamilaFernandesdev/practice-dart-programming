4274df310044177976e8b090f3928f4f582624a7
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-20 20:47:43 -0300

    Create variaveis.dart

---
+++ First steps/variaveis.dart
@@ -0,0 +1,33 @@
+void main() {
+   // Variavéis
+  
+  int codigo = 1;
+  int numero;
+  numero =3;
+  // números reais
+  // Não pode colocar acento em variável
+  double preco = 3.13;
+  String nome = 'Camila';
+  // bool é com letra minuscula
+  bool venda = false;
+  
+  // ==========================================
+  // Evite usar essas
+  // variavel nula
+  var variavel;
+  
+  // só é possivel mudar uma variavel para otra do mesmo tipo
+  // variavel int mudar para outro int
+  // exceto opções dinamicas
+  dynamic variavel12 = 1;
+  variavel12 = 'agora posso mudar';
+  
+  print(numero);
+  print(preco);
+  print(nome);
+  print(venda);
+  print(variavel);
+  print(variavel12);
+
+
+}
