e8c3b8b49fa3ec7e7c0d589346c6a1e6227d1572
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-20 21:19:13 -0300

    Create inncrementos.dart

---
+++ First steps/inncrementos.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Incrementos
+  // usado no exemplo de code app
+ 
+  int num1 = 1;
+  int num2 = 2;
+  int num3 = 3;
+  
+  num1++;
+  
+  // diferente de 
+  ++num2;
+  // executa a soma ante do print
+  
+  num3--;
+  
+  print(num1);
+  print(num2);
+  print(num3);
+  
+  
+}
