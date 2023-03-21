fe763b3098a4b2708da57ab4bbf24347cf27d61d
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-21 19:16:46 -0300

    Create loopsFor.dart

---
+++ First steps/loopsFor.dart
@@ -0,0 +1,33 @@
+void main() {
+// Loops
+  // essa merda não é python
+  //lista = [1, 32, 3, 4];
+  
+  //increment
+  for(int i=1; i<=3; i++){
+    print(i);
+  }
+  
+   print('---------------------');
+  //decrement
+  for (int i=10; i>=3; i--){
+    print(i);
+  }
+  
+  print('---------------------');
+  //increment with double
+  for(double i=1; i<=3; i+=0.5){
+    print(i);
+  }
+  
+  print('---------------------');
+  
+  int a;
+  int b;
+  for (int a=0; a<3; a++){
+    for (int b=0; b<3; b++){
+    print('$a$b');
+    print('($b,$a)');
+    }
+  }
+}
