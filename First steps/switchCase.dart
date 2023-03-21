c6d8e42078a5299e524e0b7635fcd8d1bebe27f3
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-21 18:49:17 -0300

    Create switchCase.dart

---
+++ First steps/switchCase.dart
@@ -0,0 +1,20 @@
+void main() {
+// switch and Case using also in JavaScript
+// Subtitui as elif deixando o codigo mais limpo  
+  String tela = 'Login';
+  
+  switch( tela) {
+    case 'Home':
+      print('Home Page');
+      break;
+      
+    case 'Splashscreen':
+      print('Splash Screen');
+      break;
+      
+    default:
+      print('Tela');
+      break;
+  }
+  
+}
