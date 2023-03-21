afb288a3ef080ac496e561890510b061ff96ea83
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-03-20 21:44:28 -0300

    Create conditions.dart

---
+++ First steps/conditions.dart
@@ -0,0 +1,35 @@
+void main() {
+  // Conditions if and else
+
+  String clima;
+  clima = 'Sol';
+  
+  int temperatura = 30;
+  bool sair = false;
+  
+  
+  if (clima == 'Sol' && temperatura > 20 || sair == true){
+    print('Quente demais para sair');
+  } else {
+    print('Agora posso sair, sem pegar fogo na rua!');
+  }
+  /* pARA
+   * FAZER 
+   * COMENTARIOS
+   * LONGOSSS
+   * S
+   * S
+   * 
+   * SS
+   */
+  
+  //------------------------------------------------
+  bool condicao1 = true;
+  bool condicao2 = false;
+  
+  print(condicao1? 'Teste da condição 1 ok': 'condição 1');
+  print(condicao2? 'Teste da condição 2 ok': 'condição 2');
+  print(3 == 2 ? 'sim, é maior': 'Não é maior');
+  
+  
+}
